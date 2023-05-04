134614cba71dc0ef2c7fa3b0ecb38427c516f1d4
almunawar12 <rifkialmunawar34@gmail.com>
2023-05-04 14:01:09 +0700

    create website coffeshop kenangan senja

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const navbarNav = document.querySelector('.navbar-nav');
+// ketika hamburger menu di klik
+
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle('active');
+};
+
+// klik out sidebar
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e){
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active')
+    }
+});
